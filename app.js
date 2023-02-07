e6661b979503188d73f18087f30dbb91e029a57d
Kathryn <kathrynlytle@hotmail.com>
2023-02-07 11:18:45 -0600

    m

--- app.js
+++ app.js
@@ -4,15 +4,15 @@
 
 
 
-function getMax(arr) {
-    let max = arr[0]
-   for (let i = 0; i < arr.length; ++i) {
-    if (arr[i] > max) {
-        max = arr[i];
-    }
-   } 
-   return max; 
+function getMax(arr){ 
+let max = arr[0]
+for (let i = 0; i < arr.length; ++i) {
+if (arr[i] > max) {
+    max = arr[i];
+}
+}
+return max; 
 
 }
 
-console.log(getMax([-100, -200, -300]));
+console.log(getMax([10, 20, 30]))
