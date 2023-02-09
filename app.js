bf775f69dadfa2e8e89891de239c67f85d2b5f25
Kathryn <kathrynlytle@hotmail.com>
2023-02-09 09:29:50 -0600

    m

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-function showRating(rating) {
-    let ratings = ""; 
-    for (let i = 0; i < Math.floor(rating); ++i) {
-        ratings += "*"
-        
-        if (i !== Math.floor(rating) - 1) {
-            ratings += " "
-        }
-        
-    }
+async function postsByUser(userId) {
+    const promise = await fetch("https://jsonplaceholder.typicode.com/posts");
+    
 
-    if (!Number.isInteger(rating)) {
-        ratings += " ."; 
-    }
-    return ratings; 
+    const result = await promise.json()
+
+    const posts = result.filter(element => element.userId === userId)
+
+    console.log(posts)
+    
 }
-
-console.log(showRating(4.5));
+postsByUser(4); 
